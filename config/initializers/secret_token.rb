a6d5f123c402cf71425a6cfd448a76ea7f5774ef
Dylan Robinson <dylan@dylancodes.net>
2012-03-07 18:40:57 -0500

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudSpiders::Application.config.secret_token = 'd991d414b0fce36d244ceb428f289ce9aa5e6ae2b2cc69014b165ebfc1c185b6a22de22f4e1c32c89e0220b56ca2d9cbda28b55055c4b7964fe077836bf49978'
