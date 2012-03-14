09d8f2a1fe8a195219e86a3c43ec40a3e8fa3102
Dylan Robinson <dylan@dylancodes.net>
2012-03-14 07:55:02 -0400

    Split current tasks from upcomming tasks logically. Also now auto-deletes tasks which have ended. Updated views accordingly.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,22 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+	@current = Array.new()
+	@upcoming = Array.new()
+	Event.all.map!{|checking_event|
+		if (checking_event.has_ended)
+			checking_event.destroy
+		elsif (checking_event.has_begun)
+			@current.push(checking_event)
+		else
+			@upcoming.push(checking_event)
+		end
+	}
+    #old_events = Event.where(:has_ended => true)
+	#old_events.each do |old_event|
+	#	old_event.destroy
+	#end
+
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,14 @@ class Event < ActiveRecord::Base
 	:message => 'cannot be a negative number, and must be an integer. Event not saved'
 	validates_numericality_of :priority, :greater_than_or_equal_to => 0, 
 	:message => 'cannot be a negative number, nor can it be 0. Event not saved'
+	
+	def has_begun
+		return (Time.now >= self.start_time)
+	end
+	def has_ended
+		return (Time.now > self.end_time)
+	end
+	def starts_before_ends
+		return (self.end_time > self.start_time)
+	end
 end
